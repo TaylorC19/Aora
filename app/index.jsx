1eb2c577b845057724b49caddf766f1604598005
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2024-09-07 19:50:02 +0900

    feat: created sign-in page

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { ScrollView, Text, View, Image } from 'react-native';
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
 import CustomButton from '../components/CustomButton';
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%'}}>
-        <View className="w-full justify-start items-center h-full px-4">
+        <View className="w-full justify-start items-center min-h-[85vh] px-4">
           <Image 
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -35,15 +35,13 @@ export default function App() {
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
           <CustomButton 
             title="Continue with Email"
-            handlePress={() => {}}
+            handlePress={() => router.push('/sign-in')}
             containerStyles="w-full mt-7"
           />
         </View>
       </ScrollView>
-
-      <Text>buddy 1:07:02</Text>
-
-      <StatusBar backgroundColor='#161622' style='light' />
+      {/* Shows notification bar */}
+      <StatusBar backgroundColor='#161622' style='light' /> 
     </SafeAreaView>
   );
 }
